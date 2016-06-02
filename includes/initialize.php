462bbdc2658f99a0b408be8b381f0f1e3968b773
Paul Kretschel <paul.kretschel@icloud.com>
2016-06-03 00:56:10 +0200

    Initial commit

---
+++ includes/initialize.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('URL') ? null : define('URL', 'http://localhost/template/');
+
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'xampp'.DS.'htdocs'.DS.'template'.DS);
+
+//defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'xampp'.DS.'htdocs'.DS.'template'.DS);
+
+defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'includes'.DS);
+
+//require_once(LIB_PATH.'config.php');
+
+//require_once(LIB_PATH.'functions.php');
+
+?>

0724f51f0970e784e07961c2b370087547e4d4f0
Paul Kretschel <paul.kretschel@icloud.com>
2016-06-03 00:59:44 +0200

    Correct Paths

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,12 +1,12 @@
 <?php
 
-defined('URL') ? null : define('URL', 'http://localhost/template/');
+defined('URL') ? null : define('URL', 'http://localhost/php_template/');
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'xampp'.DS.'htdocs'.DS.'template'.DS);
+defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'xampp'.DS.'htdocs'.DS.'php_template'.DS);
 
-//defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'xampp'.DS.'htdocs'.DS.'template'.DS);
+//defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'xampp'.DS.'htdocs'.DS.'php_template'.DS);
 
 defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'includes'.DS);
 

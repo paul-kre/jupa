c21a0021914d2600b0c9dae442e6ebad7e0482fb
Paul Kretschel <paul.kretschel@icloud.com>
2016-07-12 00:00:08 +0200

    Add intro

--- public/layout/intro.php
+++ public/layout/intro.php
@@ -1,3 +1,15 @@
-<div id="intro">
-    
+<div id="intro" style="background-image: url('<?php echo URL; ?>public/img/intro_background.jpg')">
+    <div class="intro-text">
+        <div class="relative">
+            <div class="intro-logo">
+                <img alt="Logo" src="<?php echo URL; ?>public/img/intro_logo.svg">
+            </div>
+            <span>
+                <h1>Julia Paschos</h1>
+                Architektur & Innenarchitektur<br>
+                Raum gestalten im Detail.
+            </span>
+
+        </div>
+    </div>
 </div>

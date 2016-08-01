030144da14171ba95ea83382073137ca283b2a03
Paul Kretschel <paul.kretschel@icloud.com>
2016-08-01 18:05:01 +0200

    Finish Content

--- public/page/einbauten.php
+++ public/page/einbauten.php
@@ -1,1 +1,41 @@
-<span>EINBAUTEN</span>
+<?php require_once LIB_PATH."initialize.php"; ?>
+
+<section>
+    <div class="photo-and-text">
+        <div class="left-small">
+            <div class="photo-container">
+                <div class="figure" style="background-image: url('<?php echo URL; ?>public/img/changeroom_01.jpg');"></div>
+            </div>
+        </div>
+        <div class="right-big">
+            <div class="text">
+                <h1>Einbauten haben verstecktes Potential.</h1>
+                <p>
+                    Durch den Stauraum, den sie schaffen, sorgen sie für optische Ruhe und Leere im Raum. Sie sind als Wandflächen wahrnehmbar und nicht als belebtes Regal. Gleichzeitig erzeugen sie in ihrem Inneren eine Ordnung der Gegenstände. Schließlich will auch die Vielfalt unseres Lebens, wollen die Gegenstände des Alltags gut untergebracht sein.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="full-size">
+    <?php buildGallery("shoes", "changeroom_details_a_01", array("changeroom_details_b_01")); ?>
+</section>
+
+<section class="text-mid">
+    <h2>Klarheit durch Ordnung im Raum.</h2>
+    <p>
+        Einbauschränke oder raumhohe Schränke, die eher als Wände empfunden werden und zurücktreten, sind vorteilhaft.<br>
+        Besonders im gewerblichen Bereich kann es gewollt sein, dass Einbauten inszeniert werden. Nischen können zu gestalterischen Blickfängen aufgewertet werden.
+    </p>
+</section>
+
+<section class="full-size">
+    <div class="big-photo">
+        <div style="background-image: url('<?php echo URL; ?>public/img/changeroom_02.jpg');" class="figure"></div>
+    </div>
+</section>
+
+<section class="full-size">
+    <?php buildGallery("shelf_01", "shelf_02"); ?>
+</section>
--- public/page/licht.php
+++ public/page/licht.php
@@ -1,1 +1,62 @@
-<span>LICHT</span>
+<?php require_once LIB_PATH."initialize.php"; ?>
+
+<section>
+    <div class="photo-and-text">
+        <div class="left-small">
+            <div class="photo-container">
+                <div class="figure" style="background-image: url('<?php echo URL; ?>public/img/bedroom_01.jpg');"></div>
+            </div>
+        </div>
+        <div class="right-big">
+            <div class="text">
+                <h1>Farbe und Licht im Zusammenspiel.</h1>
+                <p>
+                    Architektur lebt erst, wenn die Bewohner ihre Spuren durch Farbe an der Wand, Bilder oder auch Objekte hinterlassen. Das Auge will überraschenden Reizen ausgesetzt werden, die sich mit ruhigen Zonen abwechseln.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="text-mid">
+    <h2>Ein durchgängiges Konzept.</h2>
+    <p>
+        Die Farbwahl ist von Raum zu Raum abzustimmen, vorhandene Möbel und Kunstwerke sind miteinzubeziehen und zu ergänzen. Auch hier ist für mich der Lichteinfluss eine wesentliche Komponente, die bei der Farbwahl zu berücksichtigen ist.<br>
+        Hell oder dunkel, warm oder kalt, matt oder glänzend – durch die Farbwahl werden bewusst Akzente gesetzt und Stimmungen hervorgerufen.
+    </p>
+</section>
+
+<section class="full-size">
+    <?php buildGallery("bedroom_02", "bedroom_details_a_01", array("bedroom_details_b_01")); ?>
+</section>
+
+<section class="text-mid">
+    <h2>Die Planung von natürlichem und künstlichem Licht.</h2>
+    <p>
+        Das Licht suchen und ein ausgewogenes Verhältnis von natürlichem und künstlichem Licht finden – tags wie nachts – ist eine sensible Aufgabe. Denn Licht beeinflusst unser Wohlbefinden. Wandfarben und Materialien können bei unterschiedlichen Lichtverhältnissen eine vollkommen andere Wirkung erzielen.
+    </p>
+</section>
+
+<section class="full-size">
+    <?php buildGallery("stairs_01", "stairs_details_a_01", array("stairs_details_b_01", "stairs_details_c_01")); ?>
+</section>
+
+<section class="text-mid">
+    <h2>Lichtakzente.</h2>
+    <p>
+        Eine Leuchte an der richtigen Stelle kann einen Lichtpunkt setzen und ein gestalterisches Elemente darstellen.
+    </p>
+    <p>
+        Dabei bedarf jeder Raum des eigenen Lichtszenarios. Am Schreibtisch, in der Küche, im Bad, im Schlafzimmer – ans Licht werden unterschiedlichste Anforderungen gestellt.
+    </p>
+</section>
+
+<section class="full-size">
+    <div class="big-photo">
+        <div style="background-image: url('<?php echo URL; ?>public/img/kitchen_05.jpg');" class="figure"></div>
+    </div>
+</section>
+
+<section class="full-size">
+    <?php buildGallery("window_01", "window_details_a_01", array("window_details_b_01", "window_details_c_01")); ?>
+</section>
